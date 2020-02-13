debd1b3d70478ac504b2e95b0f6790d57c3c90c8
kehonda <731.kentaro@gmail.com>
2020-02-13 20:06:27 +0900

    file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -38,7 +38,22 @@ module.exports = {
         // ローダーの処理対象となるディレクトリ
         include: path.resolve(__dirname, 'src/images'),
         // 利用するローダー
-        loader: 'url-loader'
+        loader: 'url-loader',
+        options: {
+          // 画像のファイルサイズが 8 KB（8 * 1024 = 8192）以上だったら DataURL に変換せずに出力する
+          limit: 8192,
+          // 出力する DataURL に変換しなかった画像の名前
+          // [name].[ext]　は [バンドル前のファイル名].[[バンドル前のファイルの拡張子] なので
+          // 今回は background.jpg が出力される
+          name: '[name].[ext]',
+          // DataURL に変換しなかった画像の出力先
+          // デフォルトでは output.path に指定したパス（今回は public/js）に出力される
+          // 今回は public/images に出力させたいため、public/js からの相対パスを指定する
+          outputPath: '../images/',
+          // 出力されるファイルからの画像の読み込み先
+          // 今回 public/index.html から public/images の画像を読み込みたいため、以下の指定になる
+          publicPath: path => './images/' + path
+        }
       }
     ]
   }
